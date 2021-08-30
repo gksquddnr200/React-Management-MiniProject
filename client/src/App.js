1d04dc02261c27c4e5b5a445ac8972bee4a43190
gksquddnr200 <57342158+gksquddnr200@users.noreply.github.com>
2021-08-30 20:55:51 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import {withStyles} from '@material-ui/core/styles';
 import { classExpression } from '@babel/types';
-
+import  CircularProgress  from '@material-ui/core/CircularProgress';
 const styles = theme => ({
   root:{
     width: '100%',
@@ -18,16 +18,21 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1000
+  },
+  progress: {
+    margin: theme.spacing.unit*2
   }
-})
+});
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress,20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -37,6 +42,12 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed+1})
+
   }
 
   render(){
@@ -57,7 +68,13 @@ class App extends Component {
           <TableBody>
         {
         this.state.customers ? this.state.customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
-      }) : ""}
+      }) : 
+      <TableRow>
+        <TableCell colSpan="6" align="center">
+          <CircularProgress className = {classes.progress} variant="determinate" value={this.state.completed}/>
+       </TableCell>
+      </TableRow>
+      }
       </TableBody>
         </Table>
       </Paper>
